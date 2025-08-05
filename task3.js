977ef6a584e6199c52071e411845e9156c391ab8
Bertha <bis22-bmatchuka@mubas.ac.mw>
2025-08-05 11:09:56 -0700

    task3.js

---
+++ task3.js
@@ -0,0 +1,18 @@
+//data types 
+let name = "Zoe";
+//String
+let age = 23;
+//Number
+let isstudent = true;
+//Boolean
+let hobbies = ["reading", "gaming", "football"];
+//Array
+let student = { name: "zoe", age: 23, isstudent: true };
+Object
+let id = Symbol("id");
+//symbol
+let bigNumber = BigInt(1234126351273693691623);
+//BigInt
+let nothing = null;
+//null
+console.log(name, age, isstudent,hobbies, student, id, bignumer, nothing);
