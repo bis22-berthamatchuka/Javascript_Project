6004800a994e292a79b394ed9feeab3bf1fa4c11
Bertha <bis22-bmatchuka@mubas.ac.mw>
2025-08-05 04:27:04 -0700

    adding allproject files

---
+++ task2.js
@@ -0,0 +1,8 @@
+//instanceof
+let date = new Date(); // Date
+//typeof
+let count = 100; //Numeric
+//delete
+let person = { name: "zoe", age: 23}; //object
+delete person.age;
+console.log(person);
